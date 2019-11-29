e92f3e71571f509af50524fd57ad349a4841c6f0
Aydar <a.gabdrahmanov@innopolis.ru>
2019-11-29 13:33:42 +0300

    Database for Directories

--- datanode/app/models.py
+++ datanode/app/models.py
@@ -5,22 +5,27 @@ from . import db
 class Files(db.Model):
     __tablename__ = "files"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    file_path = db.Column(db.String(256))
     name = db.Column(db.String(128), unique=True)
     timestamp = db.Column(db.DateTime,default=datetime.datetime.utcnow,unique=True)
+    size = db.Column(db.Integer)
+    dir_id = db.Column(db.Integer, db.ForeignKey('directory.id'))
 
-    def __init__(self, file_path, name, timestamp):
-        self.file_path = file_path
+    def __init__(self, name, timestamp, size):
         self.name = name
         self.timestamp = timestamp
+        self.size = size
 
     def to_dict(self):
         """Export user to dictionary data structure"""
         return {
             'id': self.id,
             'name': self.name,
-            'file_path': self.file_path,
-            'timestamp': self.timestamp
+            'timestamp': self.timestamp,
+            'size': self.size
         }
+class Directory(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    path = db.Column(db.String(128), unique=True)
+    files = db.relationship('Files', lazy='dynamic')
 
 
